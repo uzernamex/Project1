9cdb4eb39e1ed1ac1d1431f9499aa9d070fae4fb
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-10 15:50:34 -0400

    updated assertEqual file

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log(`⭐️ Assertion Passed ⭐️💫😁: + " " + ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`😡 Assertion Failed ❌😡🤬: + " " + ${actual} !== ${expected}`);
+  }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-Note
-
