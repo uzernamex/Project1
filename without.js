ca7dc8cf2386d3037e52399fb82c4d2d7a513876
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-16 22:09:32 -0400

    updated code

--- without.js
+++ without.js
@@ -36,7 +36,7 @@ without([67, 44, 23], [23]);
 //Instruction
 //Implement without which will return a subset of a given array, removing unwanted elements.
 
-//This function should take in a source array and a itemsToRemove array. 
+//This function should take in a source array and a itemsToRemove array.
 //It should return a new array with only those elements from source that are not present in the itemsToRemove array.
 
 //Here are a couple examples, but please don't limit your testing to just these scenarios:
