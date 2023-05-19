4e8653380355a51d6db5d7f6f430a4d1e4353fb9
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-19 13:30:00 -0400

    update

---
+++ takeUntil.js
@@ -0,0 +1,16 @@
+/*
+https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w2/activities/229?journey_step=30
+*/
+
+
+/* Test Cases 
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+*/
