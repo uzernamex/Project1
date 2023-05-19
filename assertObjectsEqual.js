bb970aea81486e538b2b80ef53aca8ef3fc857ca
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-19 13:22:37 -0400

    updated file

---
+++ assertObjectsEqual.js
@@ -0,0 +1,5 @@
+https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w2/activities/209?journey_step=30&workbook=5
+
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+};
