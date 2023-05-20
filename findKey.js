eff22ca88e3b07c19ae68bf37d5668ec6e45da45
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 14:52:34 -0400

    updates added

--- findKey.js
+++ findKey.js
@@ -7,11 +7,11 @@ function findKey(obj, callback) {
   return undefined;
 }
 
-findKey({
+console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 1)); // => "noma"
