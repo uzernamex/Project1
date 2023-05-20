25929b7aaa63a30c0d40711fb205ea596a4156a3
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 18:58:36 -0400

    middleTest.js

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle');
+
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([0, 6, 7, 8, 9]));
+console.log(middle([11, 12, 13, 14, 15]));
+console.log(middle([41, 42, 43, 44, 45]));
+console.log(middle([88, 28, 83, 47, 53]));
+console.log(middle([111, 552, 773, 4, 50]));
+console.log(middle([99, 62, 42, 55, 106]));
+console.log(middle([-1, 2, 903, 8, 4, 12]));
+console.log(middle([10, 200, 39, 48, 555]));
+console.log(middle([416, 702, 905, 401, 533, 8]));
