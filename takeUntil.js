13d76fc8b3f782dc40a1597d000e10695b3fefa9
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 17:07:09 -0400

    refactored code

--- takeUntil.js
+++ takeUntil.js
@@ -1,9 +1,30 @@
-/*
-https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w2/activities/229?journey_step=30
-*/
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      result.push(item);
+    } else {
+      return result;
+    }
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅ Assertion Correct! ⭐️ These values are a match ✅");
+  } else {
+    console.log("❌ False Assertion; These values are not a match ❌");
+  }
+};
 
 
-/* Test Cases 
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(takeUntil(data1, results1));
+
+/* Test Cases:
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
