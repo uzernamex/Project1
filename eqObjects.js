3553ebb09ad326f7e1095894defc883f4abf2b50
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 15:38:27 -0400

    refactored code

--- eqObjects.js
+++ eqObjects.js
@@ -9,36 +9,13 @@ const assertEqual = function(actual, expected) {
 };
 /*/
 
-/*
-
-While working more with objects recently, we've created functions that return objects. 
-We realized that assertEqual can really only be used for checking primitive return types.
-
-It's time to do the same for objects now, starting with eqObjects. 
-This function implementation will require us to further practice problem solving through object key iteration again. Fun times!
-
-It would be nice to make use of our handy assertEqual function. Copy it from its source file and paste it in to the top of eqObjects.js. 
-
-Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
-
-Instruction: Copy/paste our eqArrays function into this file.
-
-Inside of our eqObjects function, we will be able to call eqArrays whenever we encounter arrays.
-
-*/
-
-const eqObjects = function(object1, object2) {
-  
+const eqObjects = function(obj1, obj2) {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  
   if (keys1.length !== keys2.length) {
     return false;
   }
   for (let key of keys1) {
-    if (!obj2.hasOwnProperty(key)) {
-      return false;
-    }
     if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
       if (!eqObjects(obj1[key], obj2[key])) {
         return false;
@@ -46,14 +23,10 @@ const eqObjects = function(object1, object2) {
         return false;
       }
     }
-    return true;
   }
+  return true;
 };
 
-
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-eqObjects(shirtObject , anotherShirtObject); // => true
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject , longSleeveShirtObject); // => false
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject , anotherShirtObject));
