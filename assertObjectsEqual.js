511e18f57bab7b90104bbc2ec5816a40b65e8048
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 15:51:19 -0400

    refactored code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,31 @@
-https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w2/activities/209?journey_step=30&workbook=5
-
-const assertObjectsEqual = function(actual, expected) {
-  // Implement me!
+const eqObjects = function(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      if (!eqObjects(obj1[key], obj2[key])) {
+        return false;
+      } else if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
+
+const assertObjectsEqual = function(obj1, obj2) {
+  if (eqObjects(obj1, obj2)) {
+    console.log("✅ Assertion Correct! ⭐️ These values are a match ✅");
+  } else {
+    console.log("❌ False Assertion; These values are not a match ❌");
+  }
+};
+
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject));
+assertObjectsEqual(shirtObject, anotherShirtObject);
