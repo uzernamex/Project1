382669efe698af1c72acc4d6007496e5ddc45b8f
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-05-20 21:10:18 -0400

    refactored code

--- without.js
+++ without.js
@@ -1,25 +1,5 @@
-/*
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`⭐️ Assertion Passed ⭐️💫😁: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`😡 Assertion Failed ❌😡🤬: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-*/
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
 
 const without = function(source, itemsToRemove) {
   let result = [];
@@ -31,20 +11,5 @@ const without = function(source, itemsToRemove) {
   console.log(result);
 };
 
-without([67, 44, 23], [23]);
 
-//Instruction
-//Implement without which will return a subset of a given array, removing unwanted elements.
-
-//This function should take in a source array and a itemsToRemove array.
-//It should return a new array with only those elements from source that are not present in the itemsToRemove array.
-
-//Here are a couple examples, but please don't limit your testing to just these scenarios:
-
-//without([1, 2, 3], [1]) // => [2, 3]
-//without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-
-//Use assertArraysEqual to write test cases for various scenarios.
-//The tests can be written below the definition of your function.
-
-
+module.exports = without; 
